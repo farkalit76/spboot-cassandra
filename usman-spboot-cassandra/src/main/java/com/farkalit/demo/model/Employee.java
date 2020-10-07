e438c6e154784570ad502863d2ec7f5d0500064b
farkalit76 <30614406+farkalit76@users.noreply.github.com>
2020-10-07 09:32:16 +0530

    updated for datetime

--- usman-spboot-cassandra/src/main/java/com/farkalit/demo/model/Employee.java
+++ usman-spboot-cassandra/src/main/java/com/farkalit/demo/model/Employee.java
@@ -1,6 +1,5 @@
 package com.farkalit.demo.model;
 
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -9,31 +8,35 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 import org.springframework.lang.NonNull;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 //import com.datastax.driver.core.LocalDate;
 
 @Table("emp")
 public class Employee {
 
-	@PrimaryKey(value ="emp_id")
+	@PrimaryKey(value = "emp_id")
 	private Integer empId;
-	
+
 	@NonNull
 	private String name;
-	
-	private String city;
-	
-	private Long phone;
-	
-	private Double salary;
-	
-	@Column(value= "dobirth")
-	private LocalDate dobirth;
-	
-	@Column(value= "created_time")
-	private LocalDateTime createdTime=  LocalDateTime.now();
 
-	public Employee() {}
-	
+	private String city;
+
+	private Long phone;
+
+	private Double salary;
+
+	@Column(value = "dobirth")
+	private LocalDate dobirth;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	@Column(value = "created_time")
+	private LocalDateTime createdTime = LocalDateTime.now();
+
+	public Employee() {
+	}
+
 	public Employee(Integer empId, String name, String city, Long phone, Double salary, LocalDate dobirth,
 			LocalDateTime createdTime) {
 		this.empId = empId;
@@ -86,7 +89,7 @@ public class Employee {
 	}
 
 	public String getDobirth() {
-		if( dobirth != null)
+		if (dobirth != null)
 			return dobirth.toString();
 		else
 			return "";
@@ -109,5 +112,12 @@ public class Employee {
 		return "Employee [empId=" + empId + ", name=" + name + ", city=" + city + ", phone=" + phone + ", salary="
 				+ salary + ", dobirth=" + dobirth + ", createdTime=" + createdTime + "]";
 	}
-	
+
+//	Comparator sortingByName = new Comparator() {
+//	
+//		   @Override
+//		   public int compare(Employee s1, Employee s2) {
+//			return s1.getEmpId().compareTo(s2.getEmpId());
+//		   }
+//		};
 }
